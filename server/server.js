4f476c7b52a9b1501c51f241f2926d380f228104
WesselBoi <mathur.saksham2005@gmail.com>
2025-06-16 21:25:39 +0530

    add client url to cors

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ connectToMongoDb(process.env.MONGO_URI)
 // Middleware
 app.use(cors(
   {
-    origin: 'http://localhost:3000',
+    origin: 'https://social-buzz-1010.vercel.app/',
     credentials: true, 
   }
 ));
