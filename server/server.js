853226e2c82a812699d2459cb777afbd23d72fdd
Saksham Mathur <mathur.saksham2005@gmail.com>
2025-07-08 00:29:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ connectToMongoDb(process.env.MONGO_URI)
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs.
   message: 'Too many requests from this IP, please try again later.',
 });
 
